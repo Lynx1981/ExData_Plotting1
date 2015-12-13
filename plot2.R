50e9f6451cd8f96bc30d9ab024e962bce288b79e
Viktorija Leonova <viktorija.leonova@gmail.com>
2015-12-13 20:59:26 +0200

    Added plots, modified scripts for d/ling files

--- plot2.R
+++ plot2.R
@@ -1,7 +1,10 @@
 setClass('myDate')
 setAs("character","myDate", function(from) as.Date(from, format="%d/%m/%Y"))
-m <- read.csv("household_power_consumption.txt", sep=";", colClasses=c("myDate", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), header=T, na.strings="?")
-n <- m[m$Date == "2007-02-01" | m$Date == "2007-02-02"]
+temp <- tempfile()
+download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile=temp, mode="wb")
+m <- read.csv(unz(temp, "household_power_consumption.txt"), sep=";", colClasses=c("myDate", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"), header=T, na.strings="?")
+unlink(temp)
+n <- m[m$Date == "2007-02-01" | m$Date == "2007-02-02",]
 n$timestamp <- as.POSIXct(paste(n$Date, n$Time))
 
 Sys.setlocale(category = "LC_ALL", locale = "english")
